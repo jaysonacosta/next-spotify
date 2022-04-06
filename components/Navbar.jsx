30e82310d9b9ac56046ab726c1fa3cdb49a7b2f8
“Jayson <jaysonacosta540@gmail.com>
2022-04-05 21:10:56 -0400

    navbar authentication and styles

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,64 @@
 // Styles
-import { wrapper, navbarElement } from '../styles/Navbar.module.css';
-import { btn } from '../styles/utils.module.css';
+import {
+	wrapper,
+	navbarElement,
+	dropdownMenu,
+	icon,
+	menuItem,
+	iconWrapper,
+} from '../styles/Navbar.module.css';
+import { btn, textWhite, textSm, textBold } from '../styles/utils.module.css';
+
+// FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
 // NextAuth
 import { useSession, signIn, signOut } from 'next-auth/react';
 
+// Hooks
+import { useState } from 'react';
+
 export default function Navbar() {
+	const [isActive, setState] = useState(false);
 	const { data: session } = useSession();
 	if (session) {
 		return (
 			<div className={wrapper}>
 				<div className={navbarElement}>
-					<button className={btn} onClick={() => signOut()}>
-						Sign Out
-					</button>
+					<div
+						className={iconWrapper}
+						onClick={() => {
+							setState(!isActive);
+						}}
+					>
+						<FontAwesomeIcon
+							className={icon}
+							icon={faCircleUser}
+							fontSize='30'
+							color='white'
+						/>
+						<span className={`${textWhite} ${textSm} ${textBold}`}>
+							{session.user.name}
+						</span>
+					</div>
+
+					{isActive && (
+						<div className={dropdownMenu}>
+							<div className={`${menuItem} ${textWhite} ${textSm}`}>
+								Profile
+							</div>
+							<div className={`${menuItem} ${textWhite} ${textSm}`}>
+								Settings
+							</div>
+							<div
+								className={`${menuItem} ${textWhite} ${textSm}`}
+								onClick={() => signOut()}
+							>
+								Sign Out
+							</div>
+						</div>
+					)}
 				</div>
 			</div>
 		);
