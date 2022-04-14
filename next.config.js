5f37aa5d448f78a6ef62ed7f92bdbf5ab34feb25
“Jayson <jaysonacosta540@gmail.com>
2022-04-14 01:05:51 -0400

    getting playlist data

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ['i.scdn.co', 't.scdn.co', 'charts-images.scdn.co'],
+		domains: [
+			'i.scdn.co',
+			't.scdn.co',
+			'charts-images.scdn.co',
+			'daily-mix.scdn.co',
+		],
 	},
 };
 
