793f59bf1beb76fc6ef9fed39fb39b8556ad46fe
Jayson Acosta <jaysonacosta@Jaysons-MacBook-Pro.local>
2022-06-05 15:59:48 -0700

    Changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	env: {
-		SPOTIFY_CLIENT_ID: 'bf963c15511f4e4ebce4961adaa3be5c',
-		SPOTIFY_CLIENT_SECRET: 'e31290b52e214313b9ddc0d029132617',
-		JWT_SECRET: 'V33emBwrKppIny0jMJLVcU/QXEHCQrk9htwj5ROR6IQ=',
-	},
 	reactStrictMode: true,
 	images: {
 		domains: [
